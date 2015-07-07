778f223690d06c5264fa47093a78683eb86381a3
Shani-08 <Shani-08@users.noreply.github.com>
2015-07-07 12:39:30 +0100

    tele

--- plugin.video.shahidmbcnet/resources/community/teledunetPlayer.py
+++ plugin.video.shahidmbcnet/resources/community/teledunetPlayer.py
@@ -116,7 +116,7 @@ def PlayStream(sourceEtree, urlSoup, name, url):
             dz=re.findall(dz_patt, link)[0]        
             ip=re.findall(ip_patt, link)[0]
             ip2=''.join(ip.split('.')[0:4])
-            token=str(long(ip2)*len(channelId)*int(dz)+int(0 +random.random() *10))
+            token=str(long(ip2)*len(channelId)*int(dz)+int(0 +random.random() *10))+'1'
             print 'dz',	dz        
             access_id=str(((365-int(dz))*long(ip2)*v1)+v2)
             access_id='?id1='+access_id
